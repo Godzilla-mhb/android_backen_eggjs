e4c6d52ba31a094c3aae535462b12d36ac6659e6
哥斯拉0 <2673648021@qq.com>
2023-09-02 21:26:30 +0800

    9/2提交

--- app/controller/firewarning.js
+++ app/controller/firewarning.js
@@ -5,6 +5,8 @@ const { Controller } = require('egg');
 class FireController extends Controller {
   async index() {
     const { ctx } = this;
+    // 关闭录像功能
+    exec('python /home/mhb/src/stream_server/kill.py film')
     exec('python /home/mhb/src/stream_server/fire.py')
     ctx.body = '火灾报警打开！';
   }
--- app/controller/video.js
+++ app/controller/video.js
@@ -5,6 +5,9 @@ const { Controller } = require('egg');
 class VideoController extends Controller {
   async index() {
     const { ctx } = this;
+    // 关闭火灾报警的摄像机占用
+    exec('python /home/mhb/src/stream_server/kill.py fire.py')
+    // 开启
     exec('python /home/mhb/src/stream_server/recordVideo.py')
     ctx.body = '开始录制！';
   }
