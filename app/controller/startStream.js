b4488651481926ab5b0060db880fa0dd4c6f7c7b
哥斯拉0 <2673648021@qq.com>
2023-09-14 14:14:19 +0800

    修改字段

--- app/controller/startStream.js
+++ app/controller/startStream.js
@@ -6,20 +6,23 @@ class StreamController extends Controller {
   async index() {
     const { ctx } = this;
     // 关闭火灾报警的摄像机占用
-    const kill = Promise((res, rej) => {
+    const kill = new Promise((res, rej) => {
       exec(
         "python /home/mhb/src/stream_server/kill.py fire.py",
         (stdout, stderr) => {
-          console.log(stdout, stderr);
+          console.log('关闭fire错误',stderr);
           res();
         }
       );
     });
 
     kill.then((res) => {
-      console.log("成功:", res);
+      console.log("关闭fire成功:", res);
     });
-    exec("sudo ./home/mhb/src/stream_server/film");
+
+    exec("sudo ./home/mhb/src/stream_server/film", (stdout, stderr) => {
+      console.log('打开film错误',stderr);
+    });
     ctx.body = "推流打开！";
   }
 }
